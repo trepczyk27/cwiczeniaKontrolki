e6edf9c53f1d04e8a00f6d434ff576459546fb38
trepczyk27 <jjd85895@gmail.com>
2021-12-07 16:06:58 +0100

    skonczona 1 i 2,3,4 rozpoczecie 5

---
+++ app/src/main/java/com/example/cwiczeniakontrolki/twojeZamownienieActivity2.java
@@ -0,0 +1,14 @@
+package com.example.cwiczeniakontrolki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class twojeZamownienieActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twoje_zamownienie2);
+    }
+}
