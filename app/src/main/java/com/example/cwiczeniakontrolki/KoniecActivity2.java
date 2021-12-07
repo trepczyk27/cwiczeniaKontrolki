13cbfdd5bb920660e740a93d66fc977c3a9e711d
trepczyk27 <jjd85895@gmail.com>
2021-12-07 16:13:40 +0100

    zakonczenie xml nastepne java

---
+++ app/src/main/java/com/example/cwiczeniakontrolki/KoniecActivity2.java
@@ -0,0 +1,14 @@
+package com.example.cwiczeniakontrolki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KoniecActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koniec2);
+    }
+}
