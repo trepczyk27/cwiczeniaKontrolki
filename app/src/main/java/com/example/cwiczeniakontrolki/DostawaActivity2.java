3f2e5fd8a5ece6897c4319e8b483818c587d34e8
trepczyk27 <jjd85895@gmail.com>
2021-12-14 14:23:59 +0100

    comit kontrolony

--- app/src/main/java/com/example/cwiczeniakontrolki/DostawaActivity2.java
+++ app/src/main/java/com/example/cwiczeniakontrolki/DostawaActivity2.java
@@ -2,13 +2,45 @@ package com.example.cwiczeniakontrolki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DostawaActivity2 extends AppCompatActivity {
+
+    private Button cofnij, dalej;
+    private EditText adres, telefon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dostawa2);
+
+        cofnij = findViewById(R.id.cofnij);
+        dalej = findViewById(R.id.dalej);
+
+        adres = findViewById(R.id.edtAdres);
+        telefon = findViewById(R.id.edtTele);
+
+
+cofnij.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent back = new Intent(DostawaActivity2.this, MainActivity.class);
+        startActivity(back);
+    }
+});
+dalej.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent next = new Intent(DostawaActivity2.this, KawaActivity2.class);
+        next.putExtra("Adres", adres.getText().toString());
+        next.putExtra("Telefon", telefon.getText().toString());
+        startActivity(next);
+    }
+});
+
     }
 }
--- app/src/main/java/com/example/cwiczeniakontrolki/MainActivity.java
+++ app/src/main/java/com/example/cwiczeniakontrolki/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.cwiczeniakontrolki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button dalej;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void zamownienie(View view) {
+        dalej = findViewById(R.id.dalej);
+
+        dalej.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DostawaActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
